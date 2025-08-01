f0173cb9c7e8fa642d575f78f73e83f980401ccc
jhue <threeax@foxmail.com>
2025-08-01 12:03:55 +0800

    fix: to build exe for build.bat

--- build.bat
+++ build.bat
@@ -6,20 +6,25 @@ set project=kohme
 REM go mod tidy
 go mod tidy
 IF ERRORLEVEL 1 (
+    pause
     exit /b 1
 )
 
 REM go generate
 go generate
 IF ERRORLEVEL 1 (
+    pause
     exit /b 1
 )
 
 REM go build
 set CGO_ENABLED=1
-go build -ldflags "-s -w" -o %project% ./cmd/bot
+go build -ldflags "-s -w" -o %project%.exe ./cmd/bot
 IF ERRORLEVEL 1 (
+    pause
     exit /b 1
 )
 
-echo build success %project%
+echo build success %project%.exe
+
+pause
